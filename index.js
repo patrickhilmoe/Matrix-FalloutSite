5abb6bd4ab2147b82a400e6a57d712b5c6cc963b
patrickhilmoe <pshilmoe@gmail.com>
2024-05-06 15:45:17 -0700

    PC additions

---
+++ index.js
@@ -0,0 +1,13 @@
+import styled, { keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+`
+const FadeInButton = styled.button`
+    animation: 1s ${fadeIn} ease-out;
+`
